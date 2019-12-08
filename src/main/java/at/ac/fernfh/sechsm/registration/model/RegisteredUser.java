b3044335226ad0ff52cdf4fab1e435d1794f4116
glasermar <mario.glaser@brz.gv.at>
2019-12-08 02:07:24 +0100

    Implement FileUpload finished with SimpleData

--- src/main/java/at/ac/fernfh/sechsm/registration/model/RegisteredUser.java
+++ src/main/java/at/ac/fernfh/sechsm/registration/model/RegisteredUser.java
@@ -1,6 +1,8 @@
 package at.ac.fernfh.sechsm.registration.model;
 
+import java.security.GeneralSecurityException;
 import java.security.PublicKey;
+import java.time.Instant;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,45 +12,69 @@ import javax.persistence.Id;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Hex;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import at.ac.fernfh.sechsm.SecHSMUtil;
 import lombok.Data;
 
 @Entity
-@Data 
+@Data
 public class RegisteredUser {
 
+	@Column
+	private Instant authTime;
 
-    @Column (length = 128) 
-    private String eMail;
-    
-    @Column (length = 4096)
-    private String encryptedTestData;
-    
-    @Column (length = 4096)
-    private String encryptedWrappingKey;
-    
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column (length = 2048)
-    private String idToken;
-    
-    
-    @JsonIgnore
-    @Transient
-    private PublicKey publicKey;
-    
-    
-    @Column (length = 1024)
-    @NotNull
-    private String pulbicKeyHash;
-    
-    
-    @Column (length = 4096)
-    @NotNull
-    private String pulbicKeyValue;
-    
+	@Column(length = 2048)
+	private String deviceInfo;
+
+	@Column(length = 128)
+	private String eMail;
+
+	@Column(length = 4096)
+	private String encryptedTestData;
+
+	@Column(length = 4096)
+	private String encryptedWrappingKey;
+
+	@Column
+	private Instant expTime;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 2048)
+	private String idToken;
+
+	@JsonIgnore
+	@Transient
+	private PublicKey publicKey;
+
+	@Column(length = 1024)
+	@NotNull
+	private String pulbicKeyHash;
+
+	@Column(length = 4096)
+	@NotNull
+	private String pulbicKeyValue;
+
+	@Column(length = 128)
+	private String subject;
+
+	public PublicKey getPublicKey() {
+		if (publicKey == null && pulbicKeyValue != null) {
+			try {
+				publicKey = SecHSMUtil.parsePublicKey(Base64.decode(pulbicKeyValue));
+			} catch (GeneralSecurityException e) {
+				throw new IllegalStateException("Already stored public key should always in the correct format to parse", e);
+			}
+		}
+		
+		return publicKey;
+	}
+
 }
+
---
+++ src/test/java/at/ac/fernfh/sechsm/registration/ModelApplicationTest.java
@@ -0,0 +1,13 @@
+package at.ac.fernfh.sechsm.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelApplicationTest.class, args);
+	}
+
+}
